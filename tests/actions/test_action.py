91d8ed0ae22a6cac0b7ae17fd338253e3c3b3790
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2026-01-13 13:20:57 +0100

    Renaming in tests

--- tests/actions/test_action.py
+++ tests/actions/test_action.py
@@ -2,9 +2,15 @@ import pytest
 from temporalio.testing import WorkflowEnvironment
 from temporalio.worker import Worker
 
-from nomad_actions.actions.entries.activities import search
-from nomad_actions.actions.entries.models import SearchWorkflowInput
-from nomad_actions.actions.entries.workflows import SearchWorkflow
+from nomad_actions.actions.entries.activities import (
+    cleanup_artifacts,
+    consolidate_output_files,
+    create_artifact_subdirectory,
+    export_dataset_to_upload,
+    search,
+)
+from nomad_actions.actions.entries.models import ExportEntriesUserInput
+from nomad_actions.actions.entries.workflows import ExportEntriesWorkflow
 
 
 @pytest.mark.asyncio
@@ -14,12 +20,18 @@ async def test_simple_workflow():
         async with Worker(
             env.client,
             task_queue=task_queue,
-            workflows=[SearchWorkflow],
-            activities=[search],
+            workflows=[ExportEntriesWorkflow],
+            activities=[
+                create_artifact_subdirectory,
+                search,
+                consolidate_output_files,
+                export_dataset_to_upload,
+                cleanup_artifacts,
+            ],
         ):
             result = await env.client.execute_workflow(
-                SearchWorkflow.run,
-                SearchWorkflowInput(
+                ExportEntriesWorkflow.run,
+                ExportEntriesUserInput(
                     upload_id='upload_id',
                     user_id='user_id',
                     query={},
